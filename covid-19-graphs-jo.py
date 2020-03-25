8856c8d1185486c2b42710c7eb08ac2b955fc6ba
Jojo4u <git@freedom-x.de>
2020-03-26 00:29:35 +0100

    add arguments, see --help; covid-19-data submodule update with new locations and upstream changes, now puts png into output dir (override with argument --no-output)

--- covid-19-graphs-jo.py
+++ covid-19-graphs-jo.py
@@ -3,8 +3,22 @@
 """
 @author: jo
 """
-indicator_name = "Confirmed" # Confirmed or Deaths
-mode = "per_capita"
+
+import argparse
+
+parser = argparse.ArgumentParser(description='COVID-19 graphs.')
+parser.add_argument('mode', choices=['per_capita', 'pct_change'], help="Mode of calculation: per capita or percent change")
+parser.add_argument('indicator', choices=['Confirmed', 'Deaths'], help="Indicator to output")
+parser.add_argument('--no-output', help="Do not save png file to output directory")
+
+args = parser.parse_args()
+mode = args.mode
+indicator_name = args.indicator
+print(f"Runnig mode '{mode}' and indicator '{indicator_name}'")
+if args.no_output is not None:
+    do_output = False
+else:
+    do_output = True
 
 if (indicator_name == "Confirmed"):
     # for per_capita plot:
@@ -26,7 +40,7 @@ elif (indicator_name == "Deaths"):
     indicator_stringoutput_singular = "death"
 
     # for confirmed and pct_change plot:
-    min_cases = 5       #only most affected countries 
+    min_cases = 20       #only most affected countries 
 
 pop_year = '2018'       #column from World Bank data,
 # for pct_change plot
@@ -39,7 +53,7 @@ ignore_countries = ['Cruise Ship']
 # San Marino has extreme numbers and skews graph
 ignore_countries_percapita = ['San Marino']
 # Always add following countries
-force_countries = ['Germany','Austria','Switzerland','France','Canada','US','Italy','Spain','Korea, South','Japan','Singapore']
+force_countries = ['Germany','Austria','Switzerland','France','US','United Kingdom','Italy','Spain','Korea, South','Japan','Singapore']
 #force_countries = ['US']
 only_forced_countries = False
 
@@ -60,7 +74,7 @@ from textwrap import fill
 
 csv_pop_countries_file = "./population/worldbank-population-2020-03-14.csv"
 csv_pop_provinces_file = "./population/province_state-population-2020-03-17.csv"
-csv_file = "./covid-19-data/time-series-19-covid-combined.csv"
+csv_file = "./covid-19-data/data/time-series-19-covid-combined.csv"
 
 def smooth(y, box_pts):
     box = np.ones(box_pts)/box_pts
@@ -96,8 +110,9 @@ df_source = df_source[~df_source['Country_Region'].isin(ignore_countries)]
 #df_result is used for filtered/processed data
 df_result = pd.DataFrame()
 
-#statistics
-print(f"Using {csv_file}, latest data from",df_source.Date.max())
+data_date = df_source.Date.max()
+print(f"Using {csv_file}, latest data from",data_date)
+
 
 actually_forced_countries = []
 actually_ignored_on_x_axis = []
@@ -193,6 +208,8 @@ if (mode == "per_capita"):
     sns.set_palette(sns.color_palette('YlOrBr_d', lines))
     sns.set_style('ticks') 
     fig, ax = plt.subplots(1,1) 
+    if (do_output):
+        fig = plt.figure(figsize=(12,7))
     
     for name,df_country in df_result.groupby('name',sort=False):
         #print(name,df_country['Confirmed (percapita)'].max())
@@ -209,7 +226,7 @@ if (mode == "per_capita"):
         # plot and put marker on last point
         plt.plot(x, y,label=name,marker='o',markevery=[length-1])
         
-    plt.tight_layout()
+    #plt.tight_layout()
     plt.title(f"COVID-19 {indicator_stringoutput_plural} per capita by country/province")
     plt.xlabel(f"Days since {start_from}th {indicator_stringoutput_singular}\nNot all days shown: " + ", ".join(actually_ignored_on_x_axis),wrap=True)
 
@@ -221,9 +238,14 @@ if (mode == "per_capita"):
     if (indicator_name == 'Confirmed'):
         ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
         ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
-    plt.legend(loc='upper left',ncol=2,framealpha=0.8)
+    plt.legend(loc='upper left',ncol=2,framealpha=0.6)
     plt.grid()
-    plt.show()
+    if (do_output):
+        output_file = "./output/" + mode + "-" + indicator_name.lower() + "-" + data_date
+        print(f"Writing png file to " + output_file)
+        fig.savefig(output_file, dpi=200, bbox_inches='tight')
+    else:
+        plt.show()
 
 elif (mode == "pct_change"):
     #lines = 0 # for color computation
@@ -233,8 +255,10 @@ elif (mode == "pct_change"):
     
     #sns.set_palette(sns.color_palette('YlOrBr_d', 10))
     #sns.set_style('ticks') 
-    #fig, ax = plt.subplots(1,1) 
-    
+    fig, ax = plt.subplots(1,1) 
+    if (do_output):
+        fig = plt.figure(figsize=(12,7))
+        
     for name,df_country in dftemp_pct.groupby('Country_Region',sort=False):
         if (only_forced_countries):
             if (name not in force_countries):
@@ -259,7 +283,12 @@ elif (mode == "pct_change"):
     plt.xticks(np.arange(math.ceil(moving_average/2),limit_x))
     plt.legend(loc='upper right',ncol=2,framealpha=1)
     plt.grid()
-    plt.show()
+    if (do_output):
+        output_file = "./output/" + mode + "-" + indicator_name.lower() + "-" + data_date
+        print(f"Writing png file to " + output_file)
+        fig.savefig(output_file, dpi=200, bbox_inches='tight')
+    else:
+        plt.show()
 
 """
 #confirmed

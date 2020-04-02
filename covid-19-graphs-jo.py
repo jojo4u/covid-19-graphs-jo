0a47d8be9f6b7386a02e14ec7b6053362310bf47
Jojo4u <git@freedom-x.de>
2020-04-02 11:33:07 +0200

    remove one level of temporary dataframes (no join), adjust ticks

--- covid-19-graphs-jo.py
+++ covid-19-graphs-jo.py
@@ -74,7 +74,9 @@ import seaborn as sns
 import numpy as np
 import math
 import warnings
+import sys
 from textwrap import fill
+
 
 csv_pop_countries_file = "./population/worldbank-population-2020-03-14.csv"
 csv_pop_provinces_file = "./population/province_state-population-2020-03-28.csv"
@@ -126,9 +128,7 @@ if (mode == "per_capita"):
     lines = 0 #for color computation
     
     #per_capita provinces
-    dftemp_provinces = df_source.copy()
-    dftemp_capita_rows = pd.DataFrame()
-    for country_province_nametpl,df_countrystate in dftemp_provinces.groupby(['Country_Region','Province_State'],sort=False):
+    for country_province_nametpl,dftemp in df_source.groupby(['Country_Region','Province_State'],sort=False):
         if has_duplicates(country_province_nametpl): # skip mainlands (covered by countries)
             continue
         if (country_province_nametpl in df_pop_provinces.index):
@@ -136,37 +136,34 @@ if (mode == "per_capita"):
         else:
             population = np.nan
             warnings.warn(f"Population for {country_province_nametpl} not found in {csv_pop_provinces_file}", stacklevel=2)
-        #print(nametuple,population)
-        dftemp = pd.DataFrame()
-        dftemp[indicator_name_percapita] = df_countrystate[indicator_name] / (population/capita)
+        dftemp = dftemp.copy()
+        dftemp[indicator_name_percapita] = dftemp[indicator_name] / (population/capita)
         dftemp['name'] = country_province_nametpl[0] + " - " + country_province_nametpl[1]
         per_capita_max = dftemp[indicator_name_percapita].max()
         if (per_capita_max >= min_percapita):
-            dftemp_capita_rows = dftemp_capita_rows.append(dftemp)
+            df_result = df_result.append(dftemp)
             lines += 1
             if not country_province_nametpl[0] in ignore_on_x_axis:
                 limit_x = len(dftemp) if len(dftemp) > limit_x else limit_x
     
-    dftemp_provinces = pd.merge(dftemp_provinces,dftemp_capita_rows,left_index=True,right_index=True)
-    df_result = df_result.append(dftemp_provinces)
-    
     #per_capita countries
-    dftemp_countries = df_source.copy()
-    dftemp_countries = dftemp_countries[~dftemp_countries['Country_Region'].isin(ignore_countries_percapita)]
-    # drop provinces/states column
-    dftemp_countries = dftemp_countries.drop(columns=['Province_State'])
-    # sum up states 
-    dftemp_countries = dftemp_countries.groupby(['Country_Region','Date'],sort=False,as_index=False).sum()
+    df_source_countries = df_source.copy()
     
-    dftemp_capita_rows = pd.DataFrame()
-    for name,df_country in dftemp_countries.groupby('Country_Region',sort=False):
+    # Removing ignored countries
+    df_source_countries  = df_source_countries[~df_source_countries['Country_Region'].isin(ignore_countries_percapita)]
+    
+    # sum up states 
+    df_source_countries  = df_source_countries.groupby(['Country_Region','Date'],sort=False,as_index=False).sum()
+    
+    for name,dftemp in df_source_countries.groupby('Country_Region',sort=False):
         if (name in df_pop_countries.index):
             population = df_pop_countries.loc[name][pop_year]
         else:
             population = np.nan
             warnings.warn(f"Population for {name} not found in {csv_pop_countries_file}", stacklevel=2)
-        dftemp = pd.DataFrame()
-        dftemp[indicator_name_percapita] = df_country[indicator_name] / (population/capita)
+        
+        dftemp = dftemp.copy()
+        dftemp[indicator_name_percapita] = dftemp[indicator_name] / (population/capita)
         dftemp['name'] = name
         per_capita_max = dftemp[indicator_name_percapita].max()
         if (only_forced_countries):
@@ -177,14 +174,10 @@ if (mode == "per_capita"):
             actually_forced_countries.append(name)
             print("Forcing", name)
         if (per_capita_max >= min_percapita or name in force_countries):
-            dftemp_capita_rows = dftemp_capita_rows.append(dftemp)
+            df_result = df_result.append(dftemp)
             lines += 1
             if not name in ignore_on_x_axis:
                 limit_x = len(dftemp) if len(dftemp) > limit_x else limit_x
-    
-    # inner join with results from per capita calculation
-    dftemp_countries = pd.merge(dftemp_countries,dftemp_capita_rows,left_index=True,right_index=True)
-    df_result = df_result.append(dftemp_countries)
     
     # need to limiting data to limit_x before sorting
     dftemp_limit = pd.DataFrame()
@@ -227,7 +220,6 @@ if (mode == "per_capita"):
         # plot and put marker on last point
         plt.plot(x, y,label=name,marker='o',markevery=[length-1])
         
-    #plt.tight_layout()
     plt.title(f"COVID-19 {indicator_stringoutput_plural} per capita by country/province (data {data_date})")
     plt.xlabel(f"Days since {start_from}th {indicator_stringoutput_singular}\nNot all days shown: " + ", ".join(actually_ignored_on_x_axis),wrap=True)
 
@@ -237,12 +229,10 @@ if (mode == "per_capita"):
 
     plt.xticks(np.arange(limit_x))
     if (indicator_name == 'Confirmed'):
-        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-        ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
-    if (indicator_name == "Confirmed"):
-        plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
-    else:
-        plt.legend(loc='upper left',ncol=2,framealpha=0.6)
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+        #ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+    plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
+    #plt.legend(loc='upper left',ncol=2,framealpha=0.6)
     plt.grid()
     if (do_output):
         output_file = "./output/" + mode + "-" + indicator_name.lower() + "-" + data_date
